da182fd11345d577eb7b3a4ddbd18d98f4a3a91c
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-20 17:04:33 +0300

    1

--- src/com/company/HomeWork3.java
+++ src/com/company/HomeWork3.java
@@ -1,9 +1,30 @@
+
+                                     //..IF..ELSE IF..SWITCH..\\
+
 package com.company;
 
-public class HomeWork3 {
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
     public static void main(String[] args) {
 
-
+        Objects.equals("Вася", "Анастасия");
+        Scanner in = new Scanner(System.in);
+        System.out.print("ВВЕДИТЕ СВОЕ ИМЯ \n");
+        String a = in.nextLine();
+        if(Objects.equals("Вася", a)){
+            System.out.println("Привет \n \"Я тебя так долго ждал\"");
+        }
+        else if(Objects.equals("Анастасия", a)){
+            System.out.println("Я тебя так долго ждал");
+        }
+        else {
+            System.out.println("Добрый день, а вы кто?");
+        }
+    }
+    public static void main(String[] args) {
 
     }
 }
